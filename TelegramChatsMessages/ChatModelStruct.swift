8566bf55760bd027747f207a1c3e3107971a9997
Reuben Simphiwe Kuse <sim.khuse@gmail.com>
2023-07-05 17:09:55 +0200

    added ChatModel Struct

---
+++ TelegramChatsMessages/ChatModelStruct.swift
@@ -0,0 +1,17 @@
+//
+//  ChatModelStruct.swift
+//  TelegramChatsMessages
+//
+//  Created by Reuben Simphiwe Kuse on 2023/07/05.
+//
+
+import Foundation
+import UIKit
+
+struct ChatModel {
+    let senderImage: UIImage
+    let senderName: String
+    let senderLastMessage: String
+    let lastMessageTimeStamp: String
+    
+}
